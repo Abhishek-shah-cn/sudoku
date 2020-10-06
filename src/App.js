584688d643a78d0a6cdf963849d14a0d880e1c97
abhishek-shah-vst-au4 <54413214+abhishek-shah-vst-au4@users.noreply.github.com>
2020-10-06 08:55:11 +0530

    Added reducers and Utils left with fixing part

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import Grid from './components/Grid';
+import { solver, isSolvable, isComplete } from './utils/sudoku';
+import { solve, clear, undo} from './actions/grid';
 
 class App extends Component {
   componentDidMount() {
@@ -12,8 +15,51 @@ class App extends Component {
   }
 
   render() {
-    
-    return <div></div>;
+    const {store} = this.props;
+		const {grid, status} = store.getState();
+		const {isSolved, isEdited} = status;
+
+    return <div>
+      <button
+					className='undo'
+					disabled={window.gridHistory && !window.gridHistory.length}
+					onClick={() => store.dispatch(undo())}
+				>
+					⤺ Undo
+				</button>
+				<button
+					className='clear'
+					disabled={!isEdited}
+					onClick={() => store.dispatch(clear())}
+				>
+					⟲ Clear
+				</button>
+
+				<Grid grid={grid} status={status} {...this.props}/>
+
+				<button
+					className='check'
+					disabled={isSolved}
+					onClick={() => {
+						if (isSolvable(grid)) {
+							if (isComplete(grid)) {
+								return alert('Congratulations, you solved it!!');
+							}
+							alert('This Sudoku is solvable, keep going !!');
+						} else {
+							alert('This Sudoku is NOT solvable');
+						}					
+					}}
+				>
+					Check
+				</button>
+				<button
+					className='solve'
+					onClick={() => store.dispatch(solve())}
+				>
+					Solve
+				</button>
+    </div>;
   }
 }
 export default App;
